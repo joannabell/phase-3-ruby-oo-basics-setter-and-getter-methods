363c80cb92fe51e8eec9c5c8542cfb630cbdb4e4
Joanna Bell <jjbdigital@gmail.com>
2022-10-11 12:09:51 -0500

    commit tree 0b8e0ecb7874a524883c0a0939ef44761cc0bd52

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    #setter method
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter method
+    def breed
+        @breed
+    end
+
+    #setter method
+    def name=(name)
+        @name = name
+    end
+    
+    #getter method
+    def name
+        @name
+    end
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+
+        # setter method
+        def job=(job)
+            @job = job
+          end
+        
+          # getter method
+          def job
+            @job
+          end
+  
+  end
+
